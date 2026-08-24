9e39fd2a90b17ba9143a4f747dec9884e3eba092
Ilham Firmansyah <iyamif@gmail.com>
2026-08-24 23:07:11 +0700

    Ignore .next folder

--- frontend/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"1/GHNLTUZYmvNYwaJur1KrNFoaUUQKqwnASAGE3rv4E=\"\n}"
--- frontend/.next/server/chunks/ssr/_next-internal_server_app_pendaftaran_page_actions_041qw16.js
+++
@@ -1,3 +0,0 @@
-module.exports=[70721,(a,b,c)=>{}];
-
-//# sourceMappingURL=_next-internal_server_app_pendaftaran_page_actions_041qw16.js.map
